d851f238ae63e0cbd8969b2dac229f3dbdd3ceda
Hebb Tommy <hebbtom@cronos.be>
2018-10-03 11:48:50 +0200

    Add project files.

---
+++ TommyTools/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TommyTools
+{
+    public class ConsoleTools
+    {
+        public static bool Run(string exercise)
+        {
+            bool result = false;
+            Console.WriteLine("Execute exercise '{0}'? (y/other) ", exercise);
+            if ((char)Console.Read() == 'y') result = true;
+            return result;
+        }
+        public static void Devider(char sign = '=', int length = 120)
+        {
+            for (int increment = 1; increment < length - 1; increment++)
+            {
+                Console.Write(sign);
+            }
+            Console.WriteLine(sign);
+        }
+    }
+}
