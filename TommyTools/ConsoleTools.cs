a1a1c124f8c2b7f230df50a2d6e7e494f0ed798b
Tommy Hebb <tommyhebb@hotmail.com>
2018-10-31 14:57:03 +0100

    GetSpecificExercisesToRun() - 1st draft

--- TommyTools/ConsoleTools.cs
+++ TommyTools/ConsoleTools.cs
@@ -16,6 +16,51 @@ namespace TommyTools
             if (isNumber == false) result = 0;
             return result;
         }
+        public static int[] GetSpecificExercisesToRun()
+        {
+            Devider();
+            int count = 0;
+            int[] exercisesArray = new int[0];
+            int[] oldExercisesArray = new int[1];
+            bool isNumber;
+            Console.WriteLine("Execute specific exercises? (next exercisenumber/other) ");
+            do
+            {
+                isNumber = int.TryParse(Console.ReadLine(), out int result);
+                if (isNumber)
+                {
+                    count++;
+                    exercisesArray = new int[count];
+                    if (count == 1)
+                    {
+                        exercisesArray[0] = result;
+                    }
+                    else
+                    {
+                        for (int index = 0; index < oldExercisesArray.Count(); index++)
+                        {
+                            exercisesArray[index] = oldExercisesArray[index];
+                        }
+                        exercisesArray[count] = result;
+                    }
+                    for (int index = 0; index < exercisesArray.Count(); index++)
+                    {
+                        oldExercisesArray[index] = exercisesArray[index];
+                    }
+                }
+                else
+                {
+                    if (count == 0)
+                    {
+                        exercisesArray = new int[1];
+                        exercisesArray[0] = 0;
+                    }
+                }
+            }
+            while (isNumber);
+
+            return exercisesArray;
+        }
         public static bool Run(string exercise)
         {
             Devider();
