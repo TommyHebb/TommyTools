37555ae31b49e825ec4b67623e7bde827419d2a5
Tommy Hebb <tommyhebb@hotmail.com>
2018-11-28 12:02:54 +0100

    Added BaseExercise

---
+++ TommyTools/BaseExercise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace TommyTools
+{
+    public abstract class BaseExercise
+    {
+        public abstract string Name { get; set; }
+
+        public abstract int Number { get; set; }
+
+        public abstract void Exec();
+
+        public void NeedsExecution(int[] exerciseNumbersThatNeedExecution)
+        {
+            if (exerciseNumbersThatNeedExecution.Contains(Number))
+            {
+                ConsoleTools.Devider();
+                Console.WriteLine("Executing exercise {0}... ", Number);
+                ConsoleTools.Devider('-', 25);
+                Exec();
+            }
+        }
+    }
+}
